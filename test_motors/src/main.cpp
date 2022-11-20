4e2668fcef7d1e9c671c1045b93f5bbcd8375966
luisaschiu <96202280+luisaschiu@users.noreply.github.com>
2022-11-19 21:37:29 -0800

    Update main.cpp

--- test_motors/src/main.cpp
+++ test_motors/src/main.cpp
@@ -40,7 +40,7 @@ void task_motor1(void* param)
         {
             Serial << "In state 0";
             motor1.fwd(255);
-            if ((encoder.getCount()/2) >= 30000)
+            if ((encoder.getCount()/2) >= 4097)
             {  
                 Serial << "Going to state 1";
                 state = 1;
