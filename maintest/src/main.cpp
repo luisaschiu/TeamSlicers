e1076d14f359896c853c2b305d300d2bd1f4db48
kevinsalceda <ksalceda@calpoly.edu>
2022-11-21 14:34:51 -0800

    Implemented FSM and task diagrams
    - shares created
    - ultrasonic, motor 1, motor 2 tasks coded
    - task user not implemented yet
    - changes made to bool names from task diagram created 11-20-2022

--- maintest/src/main.cpp
+++ maintest/src/main.cpp
@@ -14,26 +14,48 @@
 #define STBY 4
 #define CH_A 0 // Yellow Wire ENCODER 
 #define CH_B 15 // Blue Wire ENCODER 
-#define HOMELIMIT_PIN 34
+#define BLADELIMIT_PIN 34
+#define HOMEPUSHLIMIT_PIN 39
+#define ENDPUSHLIMIT_PIN 36
 #define trigPin 17
 #define echoPin 16
 
 
 Share<float> distance ("HCSR04 Output");
+Share<bool> start_flag ("Start Button"); //used to be ready_flag
+Share<bool> push_flag ("Motor Communication Bool");
+Share<bool> user_cut_flag ("User Input/Start Cutting");
+Share<bool> home_pusher_flag ("Home Pusher Bool");
+Share<bool> home_blade_flag ("Home Blade Bool");
+Share<bool> done_flag ("Slicing Finished Bool");
+Share<int> thickness_option ("Option for Different Thicknesses");
 
 PushMotor motor1 = PushMotor(BIN1, BIN2, PWMB, STBY);
 
 void task_motor1(void* param)
 {
-    int state = 2;
+    int state = 0;
     int counter = 0;
     for(;;)
     {
         distance.get();
-        if (state == 1)
+        if (state == 0)
+        {
+            motor1.stop();
+            if ((home_pusher_flag.get() == true) and (analogRead(HOMEPUSHLIMIT_PIN) == 0))
+            {
+                state = 1;
+            }
+            else if (analogRead(HOMEPUSHLIMIT_PIN) > 0)
+            {
+                state = 2;
+            }
+
+        }
+        else if (state == 1)
         {
             motor1.rev(255);
-            if (analogRead(HOMELIMIT_PIN) > 0)
+            if (analogRead(HOMEPUSHLIMIT_PIN) > 0)
             {
                 state = 2;
             }
@@ -41,52 +63,102 @@ void task_motor1(void* param)
         else if (state == 2)
         {
             motor1.stop();
-            // if ready_flag == true;
-            state = 3;
+            if (start_flag.get() == true);
+            {
+                state = 3;
+            }
         }
         else if (state == 3)
         {
             motor1.rev(255);
-            Serial << distance.get() << endl;
+        //    Serial << distance.get() << endl;
             if (distance.get() <= 2.00)  // UPDATE VALUE TO PUSH OBJECT TO BLADE
-                {
-                    state = 8;
-                }
+            {
+                state = 4;
+            }
+        }
+        else if (state == 3)
+        {
+            motor1.stop();
+            if ((user_cut_flag.get() == true) and (thickness_option.get() == 1))
+            {
+                state = 5;
+            }
+            else if ((user_cut_flag.get() == true) and (thickness_option.get() == 2))
+            {
+                state = 6;
+            }
+            else if ((user_cut_flag.get() == true) and (thickness_option.get() == 3))
+            {
+                state = 7;
+            }
         }
         else if (state == 5)
         {
+            push_flag.put(true);
             motor1.fwd(255);
             counter ++;
             if (counter == 13) // counter = 13, loaded: 0.25 in travel
             {  
+                push_flag.put(false);
                 state = 8;
-            } 
+            }
+            else if (analogRead(ENDPUSHLIMIT_PIN) > 0)
+            {
+                done_flag.put(true);
+                state = 0;
+            }
         }
         else if (state == 6)
         {
+            push_flag.put(true);
             motor1.fwd(255);
             counter ++;
             if (counter == 26) // counter = 26, loaded: 0.5 in travel
             {  
+                push_flag.put(false);
                 state = 8;
             } 
+            else if (analogRead(ENDPUSHLIMIT_PIN) > 0)
+            {
+                done_flag.put(true);
+                state = 0;
+            }
         }
         else if (state == 7)
         {
+            push_flag.put(true);
             motor1.fwd(255);
             counter ++;
             if (counter == 52) // counter = 52, loaded: 1 in travel
             {  
+                push_flag.put(false);
                 state = 8;
             } 
+            else if (analogRead(ENDPUSHLIMIT_PIN) > 0)
+            {
+                done_flag.put(true);
+                state = 0;
+            }
         }
         else if (state == 8)
         {
             motor1.stop();
-            state = 8;
-        }
-        else if (state == 9)
-        {
+            if ((thickness_option.get() == 1) and (push_flag.get() == true))
+            {
+                counter = 0;
+                state = 5;
+            }
+            else if ((thickness_option.get() == 2) and (push_flag.get() == true))
+            {
+                counter = 0;
+                state = 6;
+            }
+            else if ((thickness_option.get() == 3) and (push_flag.get() == true))
+            {
+                counter = 0;
+                state = 7;
+            }
         }
      vTaskDelay(100/portTICK_PERIOD_MS); //Delay for 100 ms
     }
@@ -96,24 +168,71 @@ BladeMotor motor2 = BladeMotor(AIN1, AIN2, PWMA, STBY);
 
 void task_motor2(void* param)
 {
-    //int state = 0;
+    int state = 0;
     for(;;)
     {
-        motor2.fwd(255);
-        vTaskDelay(20/portTICK_PERIOD_MS); //Delay for 20 ms
+        if (state == 0)
+        {
+            if (start_flag.get() == true)
+                {
+                    state = 1;
+                }
+            else if (home_blade_flag.get() == true)
+                {
+                    state = 2;
+                }
+        }
+        else if (state == 1)
+        {
+            motor2.stop();
+            home_blade_flag.put(false);
+            if (done_flag.get() == true)
+            { 
+                state = 0;
+            }
+            else if (push_flag.get() == false)
+            {
+                state = 2;
+            }
+        }
+        else if (state == 2)
+        {
+            motor2.fwd(255);
+            if (analogRead(BLADELIMIT_PIN) > 0)
+            {
+                push_flag.put(true);
+                state = 1;
+            }
+
+        }
+    vTaskDelay(20/portTICK_PERIOD_MS); //Delay for 20 ms
     }
 }
 
 HCSR04 ultrasonic = HCSR04(trigPin, echoPin);
 void task_ultrasonic(void* param)
 {
+    int state = 0;
     float measurement;
     for(;;)
     {
-        measurement = ultrasonic.measure();
-        distance.put(measurement);
-        Serial << measurement << endl;
-        vTaskDelay(30/portTICK_PERIOD_MS); //Delay for 30 ms
+        if (state == 0)
+        {
+            if (start_flag.get() == true)
+                {
+                    state = 1;
+                }
+        }
+        else if (state == 1)
+        {
+            measurement = ultrasonic.measure();
+            distance.put(measurement);
+        //    Serial << measurement << endl;
+            vTaskDelay(20/portTICK_PERIOD_MS); //Delay for 20 ms
+            if (measurement <= 0.00)
+                state = 0;
+        }
+
     }
 }
 
--- maintest/src/shares.h
+++ maintest/src/shares.h
@@ -15,6 +15,12 @@
 
 // A share which holds a counter of how many times a simulated event occurred
 extern Share<float> distance;
-
+extern Share<bool> start_flag; //used to be ready_flag
+extern Share<bool> push_flag;
+extern Share<bool> user_cut_flag;
+extern Share<bool> home_pusher_flag;
+extern Share<bool> home_blade_flag;
+extern Share<bool> done_flag;
+extern Share<int> thickness_option;
 
 #endif // _shares_h_
