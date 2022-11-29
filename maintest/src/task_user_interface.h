c429509880017f97a36f48c9dd41cdcccba0202d
cmkep <97115665+cmkep@users.noreply.github.com>
2022-11-28 17:19:56 -0800

    Create task_user_interface.h

---
+++ maintest/src/task_user_interface.h
@@ -0,0 +1,37 @@
+#ifndef task_user_interface_h
+#define task_user_interface_h
+#include <Arduino.h>
+#include <PrintStream.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+void setup_wifi (void)
+
+void HTML_header (String& a_string, const char* page_title)
+
+void handle_DocumentRoot ()
+
+void handle_NotFound (void)
+
+void handle_home_blade (void)
+
+void handle_home_push (void)
+
+void handle_home_done (void)
+
+void handle_thick_sel (void)
+
+void handle_quarter  (void)
+
+void handle_half (void)
+
+void handle_inch (void)
+
+void handle_cut (void)
+
+void handle_CSV (void)
+
+void task_webserver (void* p_params)
+
+
+#endif
