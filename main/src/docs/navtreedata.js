8b6e2ca58744db7b9c8b8677b5cea60ed74f395c
luisaschiu <96202280+luisaschiu@users.noreply.github.com>
2022-12-05 14:40:40 -0800

    Config file created

--- main/src/docs/navtreedata.js
+++ main/src/docs/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Team Slicers", "index.html", [
+  [ "TeamSlicers", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
